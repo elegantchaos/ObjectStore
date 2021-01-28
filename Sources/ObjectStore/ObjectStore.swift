8f410bc91a51ccd1d768d0c3abf57e011ffe3c50
Sam Deane <sam@elegantchaos.com>
2021-01-28 14:56:30 +0000

    Made completions escaping

--- Sources/ObjectStore/ObjectStore.swift
+++ Sources/ObjectStore/ObjectStore.swift
@@ -7,15 +7,15 @@ public protocol ObjectStore {
     typealias ErrorsCompletion = ([String:Error]) -> ()
     typealias ErrorCompletion = (Result<Void,Error>) -> ()
     
-    func save<T>(_ objects: [T], withIds ids: [String], completion: ErrorsCompletion) where T: Encodable
-    func load<T>(_ type: T.Type, withIds ids: [String], completion: ([T], [String:Error]) -> ()) where T: Decodable
-    func remove(objectsWithIds ids: [String], completion: ErrorsCompletion)
+    func save<T>(_ objects: [T], withIds ids: [String], completion: @escaping ErrorsCompletion) where T: Encodable
+    func load<T>(_ type: T.Type, withIds ids: [String], completion: @escaping ([T], [String:Error]) -> ()) where T: Decodable
+    func remove(objectsWithIds ids: [String], completion: @escaping ErrorsCompletion)
 }
 
 // Convenience extensions which take a single object instead of a list.
 
 public extension ObjectStore {
-    func save<T>(_ object: T, withId id: String, completion: ErrorCompletion) where T: Encodable {
+    func save<T>(_ object: T, withId id: String, completion: @escaping ErrorCompletion) where T: Encodable {
         save([object], withIds: [id]) { errors in
             if let error = errors.first {
                 completion(.failure(error.value))
@@ -25,7 +25,7 @@ public extension ObjectStore {
         }
     }
     
-    func load<T>(_ type: T.Type, withId id: String, completion: (Result<T,Error>) -> ()) where T: Decodable {
+    func load<T>(_ type: T.Type, withId id: String, completion: @escaping (Result<T,Error>) -> ()) where T: Decodable {
         load(type, withIds: [id]) { decoded, errors in
             if let decoded = decoded.first {
                 completion(.success(decoded))
@@ -35,7 +35,7 @@ public extension ObjectStore {
         }
     }
     
-    func remove(objectWithId id: String, completion: ErrorCompletion) {
+    func remove(objectWithId id: String, completion: @escaping ErrorCompletion) {
         remove(objectsWithIds: [id]) { errors in
             if let error = errors.first {
                 completion(.failure(error.value))
@@ -48,19 +48,19 @@ public extension ObjectStore {
 
 // Convenience extensions which infer the identifier from the object (which must conform to Identifiable)
 @available(macOS 10.15, *) public extension ObjectStore {
-    func save<T>(_ objects: [T], completion: ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
+    func save<T>(_ objects: [T], completion: @escaping ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
         save(objects, withIds: objects.map({ $0.id }), completion: completion)
     }
 
-    func save<T>(_ object: T, completion: ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
+    func save<T>(_ object: T, completion: @escaping ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
         save([object], withIds: [object.id], completion: completion)
     }
 
-    func remove<T>(_ objects: [T], completion: ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
+    func remove<T>(_ objects: [T], completion: @escaping ErrorsCompletion) where T: Encodable, T: Identifiable, T.ID == String {
         remove(objectsWithIds: objects.map({ $0.id }), completion: completion)
     }
 
-    func remove<T>(_ object: T, completion: ErrorCompletion) where T: Encodable, T: Identifiable, T.ID == String {
+    func remove<T>(_ object: T, completion: @escaping ErrorCompletion) where T: Encodable, T: Identifiable, T.ID == String {
         remove(objectWithId: object.id, completion: completion)
     }
 
